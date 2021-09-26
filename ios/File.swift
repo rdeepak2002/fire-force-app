827e8ee9d36554296f9440f524eb0b17239f52ab
rdeepak2002 <rdeepak2002@gmail.com>
2021-09-26 01:16:00 -0400

    init commit

---
+++ ios/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  MyApp
+//
+//  Created by Deepak Ramalingam on 9/26/21.
+//
+
+import Foundation
---
+++ src/components/ListView.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const ListView = () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+export default ListView;
